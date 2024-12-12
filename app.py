59dbd8018ca40a22397b7be27b1db313828c6292
sze chan <sze@hello-sze.com>
2024-12-12 18:36:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def main():
 
 
 letter_input = st.text_input("Enter Letters", key="widget", on_change=main)
-st.button("Generate Unscrambled Words", on_click=main)
+st.button("Generate Unscrambled Words", on_click=main2)
 
 placeholder = st.empty()
 
