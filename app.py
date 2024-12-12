a766561668efc8fb33b1d57c801a83d714b26b51
sze chan <sze@hello-sze.com>
2024-12-12 17:05:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,8 @@ def main():
   result = []
   with open('true-txt.csv', 'r') as words_file:
       for line in words_file:
+          if len(word) == 1:
+            continue
           word = line.strip()
           if can_spell(letters, word):
               result.append(word.lower())
