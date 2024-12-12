87f18a0e94d0f448c1eb934ad7fbc864ab3502dd
sze chan <sze@hello-sze.com>
2024-12-12 13:30:31 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+
+
+def can_spell(letters, word):
+  # reverse sort to get the blanks ('?') to the end of letters string,
+  # so that the greedy algorithm works
+  letters = sorted(letters, reverse=True)
+  word = list(word)
+
+  for letter in letters:
+      if len(word) == 0:
+          return True
+      elif letter == '?':
+          word.pop()
+      elif letter in word:
+          word.remove(letter)
+
+  return len(word) == 0
+
+
+
+#!/usr/bin/env python3
+
+
+def main(word):
+  letters = word
+
+  result = []
+  with open('scrabble.txt', 'r') as words_file:
+      for line in words_file:
+          word = line.strip()
+          if can_spell(letters, word):
+              result.append(word)
+
+  result = sorted(result, key=lambda w: len(w), reverse=True)
+
+  for word in result:
+      print(word)
+
+main("VOLIE")
+
+title = st.text_input("Enter Letters", "AEIOU")
+st.write("The current movie title is", title)
+
+#USE lowercase if using popular.txt or words.txt
+#USE caps if using scrabble.txt because source words are in caps
