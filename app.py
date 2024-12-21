917398359cf808fabc317dbd3c7576df560003da
sze chan <sze@hello-sze.com>
2024-12-21 04:42:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -95,9 +95,9 @@ if button  or letter_input:
     for word in result_5_letter:
       st.write(word.upper())
       word_meaning = dictionary.meaning('en', word.upper())
-      if word_meaning[2]:
+      if word_meaning:
         with st.popover("Get Synonym"):
-          st.write(word_meaning[2])
+          st.write(word_meaning)
   with col2:
     for word in result_4_letter:
       st.write(word.upper())
